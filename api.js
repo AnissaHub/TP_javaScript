8b54e328f00efd92d36cd97f96c088055eea9d85
AnissaBrihmat <121158683+AnissaBrihmat@users.noreply.github.com>
2026-03-12 09:44:34 +0100

    petites modifications

--- api.js
+++ api.js
@@ -1,30 +1,30 @@
-export async function loadSuggestions(todoList) {
+// api.js
+// Module pour récupérer des suggestions de tâches depuis l'API JSONPlaceholder
 
+export async function loadSuggestions(todoList) {
+    
     try {
 
+        // 1. Appel à l'API pour récupérer 5 tâches
         const response = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=5");
 
+        // 2. Transformer la réponse en format JSON
         const data = await response.json();
 
+        // 3. Parcourir chaque tâche reçue
         data.forEach(todo => {
 
+            // créer un élément li
             let li = document.createElement("li");
             li.textContent = todo.title;
 
-            let btnDelete = document.createElement("button");
-            btnDelete.classList.add("delete-btn");
-            btnDelete.textContent = "supprimer";
-
-            btnDelete.addEventListener("click", function () {
-                li.remove();
-            });
-
-            li.appendChild(btnDelete);
+            // ajouter le li dans la liste
             todoList.appendChild(li);
         });
 
     } catch (error) {
 
+        // Gestion des erreurs si l'API ne répond pas
         console.error("Erreur lors du chargement des suggestions :", error);
 
         let li = document.createElement("li");
